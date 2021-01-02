1f64770ada1f769b1e3a4ab337e6abeec8da8f46
Bintang Parama <bintang.p.dewanto@gmail.com>
2021-01-03 00:10:34 +0700

    Initial Commit

--- website/script.js
+++ website/script.js
@@ -1,5 +1,35 @@
 const video = document.getElementById('video');
 
-function StartVideo(){
-    
-}
+//Load face detection
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('./models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('./models')
+]).then(() => {
+
+    //Start Video
+    navigator.mediaDevices.getUserMedia({ audio: false, video: true })
+    .then((stream) => { video.srcObject = stream; })
+    .catch((err) => { console.error(err); });
+
+});
+
+video.addEventListener('play', () => {
+    const canvas = faceapi.createCanvasFromMedia(video);
+    document.body.append(canvas);
+    const displaySize = { width: video.width, height: video.height }
+    faceapi.matchDimensions(canvas, displaySize)
+
+    setInterval(async () => {
+        const detections = await faceapi.detectAllFaces(
+            video, new faceapi.TinyFaceDetectorOptions()
+        ).withFaceLandmarks().withFaceExpressions();
+
+        const resizedResults = faceapi.resizedResults(detections, displaySize);
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+        faceapi.draw.drawDetections(canvas, resizedResults);
+        faceapi.draw.drawFaceLandmarks(canvas, resizedResults);
+        faceapi.draw.faceExpressions(canvas, resizedResults);
+    });
+});
