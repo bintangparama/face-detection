cd4bdbb6ca2f5ecc749bdfc4cc29c6a5418dfbff
Bintang Parama <bintang.p.dewanto@gmail.com>
2021-01-03 00:21:02 +0700

    Updates

--- script.js
+++ script.js
@@ -19,17 +19,17 @@ video.addEventListener('play', () => {
     const canvas = faceapi.createCanvasFromMedia(video);
     document.body.append(canvas);
     const displaySize = { width: video.width, height: video.height }
-    faceapi.matchDimensions(canvas, displaySize)
+    faceapi.matchDimensions(canvas, displaySize);
 
     setInterval(async () => {
         const detections = await faceapi.detectAllFaces(
             video, new faceapi.TinyFaceDetectorOptions()
         ).withFaceLandmarks().withFaceExpressions();
 
-        const resizedResults = faceapi.resizedResults(detections, displaySize);
+        const resizedResults = faceapi.resizeResults(detections, displaySize);
         canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
         faceapi.draw.drawDetections(canvas, resizedResults);
         faceapi.draw.drawFaceLandmarks(canvas, resizedResults);
-        faceapi.draw.faceExpressions(canvas, resizedResults);
-    });
+        faceapi.draw.drawFaceExpressions(canvas, resizedResults);
+    }, 100);
 });
